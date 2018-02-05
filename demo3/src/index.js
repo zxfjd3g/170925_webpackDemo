5d43f4e8443cec5c056270c37152414fa82cee10
zxfjd3g <258147149@qq.com>
2018-02-05 15:12:20 +0800

    demo3

---
+++ demo3/src/index.js
@@ -0,0 +1,30 @@
+/*
+入口js
+ */
+// 引入js模块
+import $ from 'jquery'
+import {cube} from "./js/math"
+import * as atguigu from './js/atguigu.js'
+// 引入json模块
+import lessons from './assets/json/lessons.json'
+// 引入css模块
+import './assets/css/style.css'
+
+console.log(cube(3))  // 27
+
+$(function () {
+  const $app = $('#app')
+  const $ul = $('<ul>')
+  $app.append($ul)
+  lessons.forEach(lesson => {
+    $ul.append(`<li>课程名: <span class="lesson-name">${lesson.name}</span>, 时间: ${lesson.days}天</li>`)
+  })
+
+  const $button = $('<button>去尚硅谷</button>')
+  $button.click(() => {
+	if(atguigu.studyConfirm()) {
+		atguigu.goAtguigu()
+	}
+  })
+  $app.append($button)
+})
